afb9891edcd778e4b1b91936a6090a37693adafc
3A417029 <howardzhu0709@outlook.com>
2018-10-29 21:57:46 +0800

    Exercise 5 ： 設定Route前置

--- routes/web.php
+++ routes/web.php
@@ -48,10 +48,25 @@ Route::get('Hello/{name?}',function($name='Howard'){
 });
 
 /* 將 Route 取名為 hello.index */
+
 Route::get('say/{name?}',['as'=>'hello.index',function($name='Everybody'){
     return view('welcome');
 }]);
 
+/* 設定 dashboard路徑的 Route */
+
+Route::get('dashboard',function(){
+    return 'dashboard';
+});
+
+/* 設定Route 以群組包起來設定 prefix */
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return 'admindashboard';
+    });
+});
+    
     
 
     
