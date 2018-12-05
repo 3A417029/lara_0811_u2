c7969419ed82a8c2950433c959d117661b464392
3A417029 <howardzhu0709@outlook.com>
2018-12-05 10:36:58 +0800

    修正：重複的路由註解

--- routes/web.php
+++ routes/web.php
@@ -12,49 +12,6 @@
 */
 
 
-/* 設定 Route 回傳字串 */
-
-//Route::get('/',function(){
-//   return'welcome';
-//});
-
-/* 設定 Route 回傳 view */
-//Route::get('/',function(){
-//    return view('welcome');
-//});
-
-/* 設定 Route 跳轉頁面 */
-
-//Route::get('/',function(){
-//    return redirect('welcome');
-//});
-
-/* 修改 Route 接受參數 */
-
-//Route::get('hello/{name}',function($name){
-//    return'Hello,'.$name;
-//});
-
-/* 修改參數成選擇性 */
-
-//Route::get('hello/{name?}',function($name='Everybody'){
-//    return'Hello,'.$name;
-//});
-
-/* 修改參數成選擇性 */
-
-//Route::get('Hello/{name?}',function($name='Howard'){
-//    return'Hello,'.$name;
-//});
-
-
-
-/* 設定 dashboard路徑的 Route */
-
-//Route::get('dashboard',function(){
-//    return 'dashboard';
-//});
-
 /* 將 Route 取名為 hello.index */
 Route::get('say/{name?}',['as'=>'hello.index',function($name='Everybody'){
     return'Hello '.$name;
